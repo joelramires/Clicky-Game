a71f03baf30e630c8091406375e772655634586f
Joel Ramirez <joelramirez@MacBook-Pro-5.home>
2019-05-28 19:03:51 -0400

    image card

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Nav from "./components/Nav";
+import ImageCard from "./components/ImageCard";
 
 class App extends Component {
   render() {
     return (<React.Fragment>
       <Nav />
+      <ImageCard />
     </React.Fragment>
     )
   }
 }
 
-export default App;
+export default  App;
