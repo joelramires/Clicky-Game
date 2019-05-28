c66c691badee76b136c130ac9890eb369f7c3227
Joel Ramirez <joelramirez@MacBook-Pro-5.home>
2019-05-28 18:32:50 -0400

    nav bar

---
+++ clicky-game/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Nav from "./components/Nav";
+
+class App extends Component {
+  render() {
+    return (<React.Fragment>
+      <Nav />
+    </React.Fragment>
+    )
+  }
+}
+
+export default App;
