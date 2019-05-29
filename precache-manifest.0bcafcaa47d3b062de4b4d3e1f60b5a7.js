5c7221558b7abf26abcd4e2ee9f2702a27d59363
Joel Ramirez <joelramirez@MacBook-Pro-5.local>
2019-05-29 12:07:12 -0400

    Updates

---
+++ precache-manifest.0bcafcaa47d3b062de4b4d3e1f60b5a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25a28221f95ecbb7498613d37e6d6ab",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "fe0e1a201a7dd8510b06",
+    "url": "/Clicky-Game/static/css/main.3e1d29ed.chunk.css"
+  },
+  {
+    "revision": "c6bd0d203e320d08a191",
+    "url": "/Clicky-Game/static/js/2.58b98f68.chunk.js"
+  },
+  {
+    "revision": "fe0e1a201a7dd8510b06",
+    "url": "/Clicky-Game/static/js/main.91ed3301.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
