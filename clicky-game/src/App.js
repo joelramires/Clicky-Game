329fa73b1a9b2c344be07d3775718aef099fc00d
Joel Ramirez <joelramirez@MacBook-Pro-5.home>
2019-05-28 23:36:53 -0400

    added title and wrapper

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,38 +1,46 @@
 import React, { Component } from 'react';
-import Nav from "./components/Nav";
 import ImageCard from "./components/ImageCard";
+import characters from "./characters.json";
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+
 
 
 class App extends Component {
+
   state = {
     characters: characters.map(character => ({ ...character })),
     yourScore: 0,
     highScore: 0,
     yourGuess: "",
-  }
+  };
+
+
 
   handleClick = id => {
-    const chosenCharacter = this.state.characters.find(name => name.id === id);
-    console.log(chosenCharacter);
 
-    if (chosenCharacter.clicked === false) {
-      chosenCharacter.clicked = true;
-      console.log(chosenCharacter);
-      const shuffledcharacters = this.state.characters.sort((a, b) => 0.5 - Math.random());
-      this.setState({ characters: shuffledCharacters })
+    const chosenPokemon = this.state.characters.find(name => name.id === id);
+    console.log(chosenPokemon);
+
+    if (chosenPokemon.clicked === false) {
+      chosenPokemon.clicked = true;
+      console.log(chosenPokemon);
+      const shuffledCharacters = this.state.characters.sort((a, b) => 0.5 - Math.random());
+      this.setState({ character: shuffledCharacters })
       this.handleIncrement();
-      console.log(this.state.yourScore)
+      console.log(this.state.yourScore);
     }
 
     else {
 
       if (this.state.yourScore === 21) {
-        alert("Congratulations! You won the game of thrones clicky game!");
-        this.setState(({
+        alert("Congratulations! You won the game!");
+        this.setState({
           highScore: this.state.yourScore,
           yourScore: 0,
           characters: characters.map(character => ({ ...character }))
-        }));
+        });
       }
 
       else if (this.state.yourScore > this.state.highScore) {
@@ -52,21 +60,41 @@ class App extends Component {
         });
       }
     }
+
   };
 
   handleIncrement = () => {
-  this.setState({ yourScore: this.state.yourScore + 1 });
-  console.log(this.state.yourScore);
+    this.setState({ yourScore: this.state.yourScore + 1 });
+    console.log(this.state.yourScore);
   };
 
 
   render() {
-    return (<React.Fragment>
-      <Nav />
-      <ImageCard />
-    </React.Fragment>
-    )
+    return (
+      <div>
+        <Nav
+          yourScore={this.state.yourScore}
+          highScore={this.state.highScore}
+          yourGuess={this.state.yourGuess}
+        />
+        <Wrapper>
+          <Title>Click each character Pokemon once to earn points.
+    <br></br>
+            Click the same Pokemon twice and it's game over.
+    </Title>
+          {this.state.characters.map(character => (
+            <ImageCard
+              id={character.id}
+              key={character.id}
+              name={character.name}
+              image={character.image}
+              clicked={character.clicked} //
+              handleClick={this.handleClick}
+            />
+          ))}
+        </Wrapper>
+      </div>
+    );
   }
 }
-
 export default App;
